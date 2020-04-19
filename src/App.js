6f37b04592e7921666e4b2dfc280d16709a38403
Patryk Krajewski <patrykraj@interia.pl>
2020-04-19 15:13:50 +0200

    add translation module with poeditor integration

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./index.css";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import theme from "utils/theme";
 
-import { Navigation } from "components";
+import { Navigation, Wrapper, LoadingIndicator } from "components";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <GlobalStyles />
       <Router>
         <Navigation
           items={[
-            { content: "Homepage", to: "/" },
-            { content: "Budget", to: "/budget" },
+            { content: t("Homepage"), to: "/" },
+            { content: t("Budget"), to: "/budget" },
           ]}
+          RightElement={
+            <div>
+              <button onClick={() => i18n.changeLanguage("pl")}>pl</button>
+              <button onClick={() => i18n.changeLanguage("en")}>en</button>
+            </div>
+          }
         />
-        <Switch>
-          <Route path="/" exact>
-            Homepage
-          </Route>
-          <Route path="/budget">Budget page</Route>
-        </Switch>
+        <Wrapper>
+          <Switch>
+            <Route path="/" exact>
+              Homepage
+            </Route>
+            <Route path="/budget">Budget page</Route>
+          </Switch>
+        </Wrapper>
       </Router>
+    </>
+  );
+}
+
+function RootApp() {
+  return (
+    <ThemeProvider theme={theme}>
+      <React.Suspense fallback={<LoadingIndicator />}>
+        <App />
+      </React.Suspense>
     </ThemeProvider>
   );
 }
 
-export default App;
+export default RootApp;
--- src/components/Navigation/Navigation.css.js
+++ src/components/Navigation/Navigation.css.js
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
+import Wrapper from "../Wrapper";
+
 export const Container = styled.div`
   display: flex;
   background-color: ${({ theme }) => theme.colors.gray.light};
   padding: ${({ theme }) => theme.spacing.s}px 0;
+  justify-content: space-between;
+`;
+
+export const NavigationWrapper = styled(Wrapper)`
+  display: flex;
   justify-content: space-between;
 `;
 
